b876bd05b06aefed530961b28a4b5342685d44fa
KenpachiZ11 <mr.daniil.b@mail.ru>
2020-12-25 08:24:19 +0300

    ProjectSmoke18
    Add Register
    
    Co-Authored-By: Breathlessss <74593745+Breathlessss@users.noreply.github.com>
    Co-Authored-By: Andreyisk2020 <74349055+Andreyisk2020@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let delay_popup = 1000;	setTimeout("document.getElementById('overlay').style.display='block'", delay_popup);
+
+function placeClick() {
+    register=window.open("register.html", "display_ford",
+      "width=400,height=500,status=no,toolbar=no,menubar=no");
+  }  
