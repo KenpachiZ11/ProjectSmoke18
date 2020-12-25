0ad4a2a3fad3cc6aa71061d6fa223a93380e1311
KenpachiZ11 <mr.daniil.b@mail.ru>
2020-12-25 11:18:13 +0300

    ProjectSmoke18
    Co-Authored-By: Andreyisk2020 <74349055+Andreyisk2020@users.noreply.github.com>
    Co-Authored-By: Breathlessss <74593745+Breathlessss@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let delay_popup = 1000;	setTimeout("document.getElementById('overlay').style.display='block'", delay_popup);
+let upBtn = document.getElementsByClassName("upBtn");
+
 
 function placeClick() {
     register=window.open("register.html", "display_ford",
-      "width=400,height=500,status=no,toolbar=no,menubar=no");
-  }  
+      "min-width=300, min-height=500,status=no,toolbar=,menubar=no");
+}
